2c7cf9e8a67aadc2eecce4629afdb8d2d412705c
Joseph Langford <joseph.p.langford@gmail.com>
2024-06-22 20:59:30 -0400

    Added token.txt check, loading, and writing logic for temporary insecure storage of OAuth token. Will do it correctly later

--- TwitchModConsole/Program.cs
+++ TwitchModConsole/Program.cs
@@ -7,7 +7,26 @@ namespace TwitchModConsole;
 public static class Program {
     static async Task Main(string[] args)
     {
-       AnsiConsole.WriteLine("Welcome to TwitchModConsole :)");
-       await TwitchOAuth.GetOAuthToken();
+        AnsiConsole.WriteLine("Welcome to TwitchModConsole :)");
+        
+        string? token = null;
+        if (File.Exists("token.txt"))
+        {
+            token = File.ReadAllText("token.txt");
+        }
+
+        if (token is null) {
+            token = await TwitchOAuth.GetOAuthToken();
+            if (token is null)
+            {
+                //TODO: Create Custom Error
+                AnsiConsole.MarkupLine("[red]Error: Unable to get OAuth token. Exiting...[/]");
+                Environment.Exit(1);
+            }
+
+            File.WriteAllText("token.txt", token);
+        }
+
+        await TwitchOAuth.GetOAuthToken();
     }
 }
